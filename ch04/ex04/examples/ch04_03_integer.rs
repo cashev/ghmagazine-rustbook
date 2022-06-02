5de39555098f3fc24a54e293066362f6a83b67a5
cashev <42924021+cashev@users.noreply.github.com>
2022-06-03 06:45:49 +0900

    integer2

--- ch04/ex04/examples/ch04_03_integer.rs
+++ ch04/ex04/examples/ch04_03_integer.rs
@@ -17,4 +17,15 @@ fn main() {
     println!("n3: {}", type_of(n3));
     println!("n4: {}", type_of(n4));
     println!("n5: {}", type_of(n5));
+
+    let h1 = 0xff;
+    let o1 = 0o744;
+    let b1 = 0b1010_0110_1110_1001;
+    println!("h1: {}, val: {}", type_of(h1), h1);
+    println!("o1: {}, val: {}", type_of(o1), o1);
+    println!("b1: {}, val: {}", type_of(b1), b1);
+
+    let n6 = b'A';
+    assert_eq!(n6, 65u8);
+    println!("n6: {}, val: {}", type_of(n6), n6);
 }
