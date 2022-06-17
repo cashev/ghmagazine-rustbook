7d1aacb548149a1faf85beab704f9f3c39704a68
cashev <42924021+cashev@users.noreply.github.com>
2022-06-18 07:11:14 +0900

    fn pointer

--- ch04/ex04/examples/ch04_11_fn_pointer.rs
+++ ch04/ex04/examples/ch04_11_fn_pointer.rs
@@ -1,3 +1,10 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> String {
+    let a = type_name::<T>();
+    a.to_string()
+}
+
 fn double(n: i32) -> i32 {
     n + n
 }
@@ -9,7 +16,13 @@ fn abs(n: i32) -> i32 {
 fn main() {
     let mut f: fn(i32) -> i32 = double;
     assert_eq!(f(-42), -84);
+    assert_eq!(std::mem::size_of_val(&f), std::mem::size_of::<usize>());
 
     f = abs;
     assert_eq!(f(42), 42);
+
+    let mut f_bad = double;
+    println!("f_bad: {}", type_of(f_bad));
+    println!("abs: {}", type_of(abs));
+    assert_eq!(std::mem::size_of_val(&f_bad), 0);
 }
