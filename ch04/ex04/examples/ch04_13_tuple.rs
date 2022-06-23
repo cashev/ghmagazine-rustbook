574825f53d59ca3eec393f06bc591bc7774e1cec
cashev <42924021+cashev@users.noreply.github.com>
2022-06-24 07:21:46 +0900

    tuple パターンマッチ

--- ch04/ex04/examples/ch04_13_tuple.rs
+++ ch04/ex04/examples/ch04_13_tuple.rs
@@ -6,4 +6,16 @@ fn main() {
     let mut t1 = (88, true);
     t1.0 += 100;
     assert_eq!(t1.0, 188);
+
+    let (n1, b1) = (88, true);
+    assert_eq!(n1, 88);
+    assert_eq!(b1, true);
+
+    let ((x1, y1), (x2, y2)) = ((0, 5), (10, -1));
+    assert_eq!(x1, 0);
+    assert_eq!(y1, 5);
+    assert_eq!(x2, 10);
+    assert_eq!(y2, -1);
+
+    let ((x1, y1), _) = ((0, 5), (10, -1));
 }
